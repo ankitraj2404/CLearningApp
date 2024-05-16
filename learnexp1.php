bcddd4a6a3aca19edbb51139bbe553c68f3491f4
ankitraj2404 <113232930+ankitraj2404@users.noreply.github.com>
2024-05-16 09:14:00 +0530

    Add files via upload

---
+++ learnexp1.php
@@ -0,0 +1,87 @@
+<?php
+// Include the session.php file to enforce session-based authentication
+include('session.php');
+?>
+<DOCTYPE html> 
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" name="viewport" content="width=device-width,initial-scale=1.0"> 
+    <title>javaboi</title>
+    <link rel="stylesheet" href="styles1.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    </head>
+    <header>
+    <h2 class="logo"><i class="fas fa-graduation-cap" id="cap"></i>C LEARNING APP</h2>
+        <nav class="navigation"> <a href="competency.php" class="btnlogout">Home</a>
+                  <a href="#">About</a>                
+                  <a href="#">Contact</a>
+                  <a href="login.html" class="btnlogout">LOG OUT</a> 
+ 
+             </nav>
+   </header>
+    
+    <body>
+       
+      <div class="video">
+        <section class="video-section">
+
+            <article class="video-container">
+                <a href="https://youtu.be/mAtkPQO1FcA?feature=shared" class="thumbnail" data-duration="10.00">
+                    <img class="thumbnail-image" src="javaboilogo.png" > 
+                </a>
+                <div class="video-bottom-section">
+                    <a href="#">
+                        <div class="video-detail">
+                            <a href="#" class="video-title"></a>
+                        </div>
+                    </a>
+                </div>
+            </article>
+
+            <article class="video-container">
+                <a href="#" class="thumbnail" data-duration="10.00">
+                    <img class="thumbnail-image" src="javaboilogo.png" > 
+                </a>
+                <div class="video-bottom-section">
+                    <a href="#">
+                        <div class="video-detail">
+                            <a href="#" class="video-title"></a>
+                        </div>
+                    </a>
+                </div>
+            </article>
+
+            <article class="video-container">
+                <a href="#" class="thumbnail" data-duration="10.00">
+                    <img class="thumbnail-image" src="javaboilogo.png" > 
+                </a>
+                <div class="video-bottom-section">
+                    <a href="#">
+                        <div class="video-detail">
+                            <a href="#" class="video-title"></a>
+                        </div>
+                    </a>
+                </div>
+            </article>
+
+            <article class="video-container">
+                <a href="#" class="thumbnail" data-duration="10.00">
+                    <img class="thumbnail-image"  type="png" src="javaboilogo.png" > 
+                </a>
+                <div class="video-bottom-section">
+                    <a href="#">
+                        <div class="video-detail">
+                            <a href="#" class="video-title"></a>
+                        </div>
+                    </a>
+                </div>
+            </article>
+
+        </section>
+      </div>
+      <div class="quizz-section">
+        <a href="index.php" class="quizz-button">
+        <button class="quizz">START QUIZZ</button></a>
+      </div>
+    
